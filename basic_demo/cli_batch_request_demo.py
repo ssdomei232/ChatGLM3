c14278394fbbb3fc90f80ac868c973bb3d78f3fb
mei <i@mmeiblog.cn>
2024-02-17 09:54:00 +0800

    Update cli_batch_request_demo.py

--- basic_demo/cli_batch_request_demo.py
+++ basic_demo/cli_batch_request_demo.py
@@ -3,7 +3,7 @@ import platform
 from typing import Optional, Union
 from transformers import AutoModel, AutoTokenizer, LogitsProcessorList
 
-MODEL_PATH = os.environ.get('MODEL_PATH', 'THUDM/chatglm3-6b')
+MODEL_PATH = 'chatglm3-6b'
 TOKENIZER_PATH = os.environ.get("TOKENIZER_PATH", MODEL_PATH)
 
 tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH, trust_remote_code=True)
